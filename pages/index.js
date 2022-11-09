a61b62b1530d5c9a8cbe9ee96bc4f620ab83f103
irllamartins <irllamartinsbarbosa@gmail.com>
2022-11-09 02:35:21 -0300

    finalizado o video 1

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,41 @@
-import config from"../config.json"
+import config from "../config.json"
 import styled from "styled-components"
+import { CSSReset } from "../src/components/CSSReset"
+import {StyledTimeline} from "../src/components/Timeline"
+import Menu from "../src/components/Menu"
 function HomePage() {
-    return( 
-    <div>
-        <Menu/>
-        <Header/>
-        <TimeLine/>
-    </div>
-    )
-  }
-  
-  export default HomePage
 
-  function Menu() {
-    return( 
-    <div>
-        
-    </div>
-    )
-  }
-  
-const styledHeader = styled.div`
+  return (
+    <>
+            <CSSReset />
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                // backgroundColor: "red",
+            }}>
+                <Menu />
+                <Header />
+                <TimeLine playlists={config.playlists}>
+                    Conteúdo
+                </TimeLine>
+            </div>
+        </>
+    
+  )
+}
+
+export default HomePage
+
+
+const StyledHeader = styled.div`
 img{
     width: 80px;
     height: 80px;
     border-radius: 50%;
   }
-  .user-infor{
+  .user-info{
+    margin-top: 50px;
     display: flex;
     align-items: center;
     width: 100%;
@@ -34,25 +43,60 @@ img{
     gap: 16px;
   }
 `;
-  function Header() {
-    return( 
-    <styledHeader>
-        <img src="banner"/>
-        <section className="user-info">
-        <img src={`https://github.com/${config.github}.png`}/>
+function Header() {
+  return (
+    <StyledHeader>
+      {/* <img src="banner"/>*/}
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
         <div>
-            <h2>{config.name}</h2>
-            <p>{config.job}</p>
-        </div>   
-        </section>
-        
-    </styledHeader>
-    )
-  }
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
 
-  function TimeLine() {
-    return( 
-    <div>Welcome to TimeLine!</div>
-    )
-  }
-  
+    </StyledHeader>
+  )
+}
+
+function TimeLine(props) {
+  const playlistNames = Object.keys(props.playlists);
+  console.log(playlistNames)
+  //Statement
+  //Retorno por expressão
+  return (
+    <StyledTimeline>
+
+      <section>
+        <h2>
+          {playlistNames.map((playlistName) => {
+            const videos = props.playlists[playlistName]
+            console.log(videos)
+            return (
+              <section>
+                <h2>{playlistName}</h2>
+                <div>
+                  {
+                    videos.map((video) => {
+                      return (
+                        <a href={video.url}>
+                          <img src={video.thumb} />
+
+                          <span>
+                            {video.title}
+                          </span>
+                        </a>
+                      )
+                    })
+                  }
+                </div>
+              </section>
+            )
+          })}
+        </h2>
+
+      </section>
+
+    </StyledTimeline>
+  )
+}
