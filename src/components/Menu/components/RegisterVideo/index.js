8957e786d93a680ef792e6e70da35b0b1a41c3d1
irllamartins <irllamartinsbarbosa@gmail.com>
2022-11-13 03:32:29 -0300

    finalizando video 4

--- src/components/Menu/components/RegisterVideo/index.js
+++ src/components/Menu/components/RegisterVideo/index.js
@@ -1,34 +1,58 @@
 import { StyledRegisterVideo } from "./styles"
 import React from "react"
 
+//custom hook
+function useForm(propsDoForm) {
+    const [values, setValues] = React.useState(propsDoForm.initialValues);
+
+    return {
+        values,
+        handleChange: (evento) => {
+            console.log(evento.target);
+            const value = evento.target.value;
+            const name = evento.target.name
+            setValues({
+                ...values,
+                [name]: value,
+            });
+        },
+        clearForm() {
+            setValues({});
+        }
+    };
+}
+
 export default function RegisterVideo() {
+    const formCadastro = useForm({ initialValues: {titulo:"",url:""}});
     const [formVisivel, setFormVisivel] = React.useState(false);
-    const [values, setValues] = React.useState({titulo:"",url:""});
     return (
         <StyledRegisterVideo>
 
-            <button className="add-video" onClick={()=>setFormVisivel(true)} >
+            <button type="button" className="add-video" onClick={()=>setFormVisivel(true)} >
                 +
             </button>
             {
                 formVisivel
                     ? (
-                        <form>
+                        <form onSubmit={(evento)=>{
+                            evento.preventDefault();
+                            setFormVisivel(false);
+                            formCadastro.clearForm();
+                        }}>
                             <div>
                                 <button className="close-modal" onClick={()=>setFormVisivel(false)} >
                                     X
                                 </button>
-                                <input placeholder="Titulo do video" value={values.titulo} onChange={(evento)=>
-                                   {
-                                     const value = evento.target.value;
-                                     setValues({
-                                        ...values,
-                                        titulo: value,
-                                     })
-                                }
-
-                                }/>
-                                <input placeholder="URL" value={values.url}/>
+                                <input
+                                 placeholder="Titulo do video" 
+                                 name="Titulo"
+                                 value={formCadastro.values.titulo}
+                                  onChange={formCadastro.handleChange}/>
+                                <input 
+                                placeholder="URL" 
+                                name="URL"
+                                value={formCadastro.values.url}
+                                 onChange={formCadastro.handleChange}/>
                                 <button type="submit">
                                     Cadastrar
                                 </button>
